28c70af4581a1af2d2a3aff83c05bba8e249d810
Jason White <jasonawhite@gmail.com>
2020-10-29 22:30:49 -0400

    testing functions

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,16 @@
 exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    message: JSON.stringify({message: 'Success'})
+  if (event.httpMethod !== 'GET') {
+    return {
+      statusCode: 405,
+      body: 'Method not allowed.'
+    }
+  }
+  try {
+    const returnObj = { statusCode: 200, body: 'derp.' }
+    console.log('Success.')
+    return returnObj
+  } catch (error) {
+    console.error(error)
+    return { statusCode: 500, body: 'Error' }
   }
 }
