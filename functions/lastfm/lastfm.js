e95c77389b018d6c4883c34fee6d47f05d9637e1
Jason White <jasonawhite@gmail.com>
2020-11-02 22:36:46 -0500

    trying something new

--- functions/lastfm/lastfm.js
+++ functions/lastfm/lastfm.js
@@ -1,14 +1,57 @@
+// for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
+const { LAST_FM_KEY } = process.env
+const ENDPOINT$ = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=qbertqbert&api_key=${LAST_FM_KEY}&format=json&limit=5`
+
+const fetch = require('node-fetch')
+const handler = async function (event, context) {
+  if (!context.clientContext && !context.clientContext.identity) {
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({
+        msg: 'No identity instance detected. Did you enable it?'
+      })
+    }
+  }
+  const { identity, user } = context.clientContext
+  try {
+    const response = await fetch(ENDPOINT$)
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: response.status, body: response.statusText }
+    }
+    const data = await response.json()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ identity, user, msg: data.value })
+    }
+  } catch (error) {
+    // output to netlify function log
+    console.log(error)
+    return {
+      statusCode: 500,
+      // Could be a custom message or object i.e. JSON.stringify(err)
+      body: JSON.stringify({ msg: error.message })
+    }
+  }
+}
+
+module.exports = { handler }
+
+/*
 const fetch = require('node-fetch')
 const { LAST_FM_KEY } = process.env
 
 const ENDPOINT = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=qbertqbert&api_key=${LAST_FM_KEY}&format=json&limit=5`
-let lastfm = null
 
+let lastfm = document.getElementById('#lastfm')
 const handler = async (event, context) => {
   let response
   try {
     response = await fetch(ENDPOINT, { headers: {'Accept': 'application/json'} })
       .then((response) => {
+        console.log(JSON.stringify(response))
         return response.json()
       })
       .then((data) => {
@@ -27,8 +70,6 @@ const handler = async (event, context) => {
                </li>`).join('')}
              </ul>
            `
-        lastfm = list
-        console.log(lastfm)
       })
   } catch (err) {
     return {
@@ -48,3 +89,4 @@ const handler = async (event, context) => {
 }
 
 module.exports = { handler }
+*/
