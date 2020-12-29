3f5d9f79725b9a4c48a76ff79bdf27c4ae3ad8fe
Jason White <jasonawhite@gmail.com>
2020-12-28 22:26:28 -0500

    editing functions

--- functions/helpers/getTracks.js
+++ functions/helpers/getTracks.js
@@ -1,9 +1,7 @@
 const key = JSON.stringify(process.env.LAST_FM_KEY)
 const formattedReturn = require('./formattedReturn')
 
-const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=qbertqbert&api_key=${key}&format=json&limit=5&cb=http://jasonwhite.us`
-
-// let lastfm = document.getElementById('lastfm')
+const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=qbertqbert&api_key=${key}&format=json&limit=5`
 
 module.exports = async (event) => {
   try {
@@ -16,7 +14,8 @@ module.exports = async (event) => {
           title: track.name,
           url: track.url
         }))
-        return tracks.json
+        console.log(tracks)
+        return formattedReturn(200, tracks)
       })
   } catch (err) {
     console.error(err)
