6236d3983802a39a171dc8c1c2343b4c45999727
Jason White <jasonawhite@gmail.com>
2021-02-13 22:06:03 -0500

    got netlify function working with lastfm api

--- functions/lastfm.js
+++ functions/lastfm.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 
 exports.handler = async (event, context) => {
-  const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=qbertqbert&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=5`;
+  const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=qbertqbert&api_key=${process.env.LASTFM_API_KEY}&format=json&limit=10`;
 
   const options = {
     method: "GET",
@@ -18,12 +18,11 @@ exports.handler = async (event, context) => {
   })
     .then((res) => res.json())
     .then(json => {
-      console.log(json);
-    })
-    .then(json => {
       return {
         statusCode: 200,
         body: JSON.stringify(json),
       }
+    }).catch((err) => {
+      console.error(err)
     });
 }
