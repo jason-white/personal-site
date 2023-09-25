c8e3cdcbe59a77e07eeb9d7ae08f9ae57c3dec36
Jason <jasonawhite@gmail.com>
2023-09-24 20:51:19 -0400

    reconfigured netlify functions, fixed pinboard links by using the API rather than plugin that stopped working

---
+++ static/js/spotify.js
@@ -0,0 +1,39 @@
+// Get Spotify recent tracks
+const tracks = document.getElementById("recent-tracks");
+document.body.onload = getTracks();
+
+function getTracks() {
+	return fetch('/api/spotify', {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json"
+		}
+	})
+		.then((res) => res.json())
+		.then(res => {
+			res.items.forEach((track) => {
+				tracks.innerHTML += `
+					<li>
+						<div class="track">
+							<div class="track__img">
+								<img src="${track.track.album.images[1].url}" alt="Cover art for ${track.track.artists[0].name}'s album titled ${track.track.album.name}">
+							</div>
+							<div class="track__info">
+								<a href="${track.track.artists[0].external_urls.spotify}" target="blank_">
+									${track.track.artists[0].name}
+								</a>
+								<p>${track.track.name}</p>
+								<a href="${track.track.album.external_urls.spotify}" target="blank_">
+									${track.track.album.name}
+								</a>
+							</div>
+						</div>
+					</li>
+				`
+			});
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+}
+
