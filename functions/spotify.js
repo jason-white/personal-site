3f8cf4988ebc659fd75add4b4d176c506e72a289
Jason <jasonawhite@gmail.com>
2023-09-24 21:08:40 -0400

    changed import for node fetch in netlify functions

--- functions/spotify.js
+++ functions/spotify.js
@@ -1,5 +1,4 @@
-//import fetch from "node-fetch";
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
 exports.handler = async (event, context) => {
   const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
