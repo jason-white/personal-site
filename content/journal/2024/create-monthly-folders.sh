cfb7d58d913901f6ec8529226a6d02cdc100e502
Jason <jasonawhite@gmail.com>
2024-10-31 10:19:31 -0400

    removed all generated images

--- content/journal/2024/create-monthly-folders.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# Loop from 1 to 12 to create folders named 01 to 12
-for month in {01..12}; do
-  mkdir -p "$month"
-  echo "Created folder: $month"
-done
-
-echo "All monthly folders created."
